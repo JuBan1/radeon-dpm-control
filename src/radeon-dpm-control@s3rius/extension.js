25327b23ca97b95476395b891a39b5337ed10451
Thomas Debesse <dev@illwieckz.net>
2016-01-17 01:25:26 +0100

    don't use pkexec if “dpm-query test” does not answer “forbidden”

--- src/radeon-dpm-control@s3rius/extension.js
+++ src/radeon-dpm-control@s3rius/extension.js
@@ -24,6 +24,7 @@ function enable()
 {
   dpmc = new DPMControl();
 }
+
 function disable()
 {
     dpmc.destroy();
@@ -43,9 +44,9 @@ PopupIconMenuItem.prototype =
     {
         PopupMenu.PopupBaseMenuItem.prototype._init.call(this, params);
 
-        this.box = new St.BoxLayout({ style_class: 'popup-combobox-item' });
+        this.box = new St.BoxLayout({ style_class: "popup-combobox-item" });
 
-        this.icon = new St.Icon({ gicon: gicon, style_class: 'popup-menu-icon' });
+        this.icon = new St.Icon({ gicon: gicon, style_class: "popup-menu-icon" });
         this.box.add(this.icon);
 
         this.label = new St.Label({ text: text });
@@ -55,7 +56,8 @@ PopupIconMenuItem.prototype =
         this.actor.add(this.box);
     },
 
-    _onDestroy: function(){
+    _onDestroy: function()
+    {
         this.menu.removeAll();
     }
 };
@@ -73,19 +75,23 @@ DPMControl.prototype =
     {
         PanelMenu.Button.prototype._init.call(this, 0);
 
-        this._iconActor = new St.Icon({ icon_name: 'radeon-control-green',
-                                        style_class: 'system-status-icon' });
+        this._iconActor = new St.Icon({ icon_name: "radeon-control-green",
+                                        style_class: "system-status-icon" });
 
         this.actor.add_actor(this._iconActor);
-        this.actor.add_style_class_name('panel-status-button');
+        this.actor.add_style_class_name("panel-status-button");
 
         this._display();
 
-        Main.panel.addToStatusArea('radeon-dpm-control', this);
+        Main.panel.addToStatusArea("radeon-dpm-control", this);
     },
 
-    _updateIcon: function(){
-        [res,pid,fdin,fdout,fderr] = GLib.spawn_async_with_pipes(null, ["/usr/bin/dpm-query", "get", "default"], null, GLib.SpawnFlags.SEARCH_PATH, null);
+    _updateIcon: function()
+    {
+        let dpmquery_path = GLib.find_program_in_path("dpm-query");
+
+        [res, pid, fdin, fdout, fderr] = GLib.spawn_async_with_pipes(null, [dpmquery_path, "get", "default"],
+                                                                     null, GLib.SpawnFlags.SEARCH_PATH, null);
         let outstream = new Gio.UnixInputStream({fd:fdout,close_fd:true});
         let stdout = new Gio.DataInputStream({base_stream: outstream});
 
@@ -98,52 +104,73 @@ DPMControl.prototype =
         else {
             let result = out.toString() + out2.toString();
             
-            if( result.search("low") != -1 && result.search("battery") != -1 ){
+            if( result.search("\"low\"") != -1 && result.search("\"battery\"") != -1 ){
                 this._iconActor.icon_name = "radeon-control-green";
-            } else if( result.search("auto") != -1 && result.search("balanced") != -1 ) {
+            } else if( result.search("\"auto\"") != -1 && result.search("\"balanced\"") != -1 ) {
                 this._iconActor.icon_name = "radeon-control-yellow";
-            } else if( result.search("high") != -1 && result.search("\"performance\"") != -1 ) {
+            } else if( result.search("\"high\"") != -1 && result.search("\"performance\"") != -1 ) {
                 this._iconActor.icon_name = "radeon-control-red";
             } else{ 
                 this._iconActor.icon_name = "radeon-control-other";
             }
         }
-
     },
 
    _display: function()
    {
         this._updateIcon();
 
-        let gicon = Gio.content_type_get_icon('radeon-control-green');
+        let gicon = Gio.content_type_get_icon("radeon-control-green");
         let menuItem = new PopupIconMenuItem(gicon, " Set Low/Battery", {});
         this.menu.addMenuItem(menuItem);
-        menuItem.connect('activate', Lang.bind(this, this._setDPM, 'low battery'));
+        menuItem.connect("activate", Lang.bind(this, this._setDPM, ["low", "battery"]));
 
-        let gicon = Gio.content_type_get_icon('radeon-control-yellow');
+        let gicon = Gio.content_type_get_icon("radeon-control-yellow");
         let menuItem = new PopupIconMenuItem(gicon, " Set Auto/Balanced", {});
         this.menu.addMenuItem(menuItem);
-        menuItem.connect('activate', Lang.bind(this, this._setDPM, 'auto balanced'));
+        menuItem.connect("activate", Lang.bind(this, this._setDPM, ["auto", "balanced"]));
 
-        let gicon = Gio.content_type_get_icon('radeon-control-red');
+        let gicon = Gio.content_type_get_icon("radeon-control-red");
         let menuItem = new PopupIconMenuItem(gicon, " Set High/Performance", {});
         this.menu.addMenuItem(menuItem);
-        menuItem.connect('activate', Lang.bind(this, this._setDPM, 'high performance'));
+        menuItem.connect("activate", Lang.bind(this, this._setDPM, ["high", "performance"]));
 
         event = GLib.timeout_add_seconds(0, 5, Lang.bind(this, function () {
             this._updateIcon();
             return true;
         }));
-
-
     },
 
-    //This code was taken and modified from:
-    //http://stackoverflow.com/questions/10099593/gnome-shell-privilege-escalation
-    _setDPM: function(a,b,setting)
+    // This code was taken and modified from:
+    // http://stackoverflow.com/questions/10099593/gnome-shell-privilege-escalation
+    _setDPM: function(a, b, settings)
     {
-        let pkexec_path = GLib.find_program_in_path('pkexec');
-        let result = Util.trySpawnCommandLine(pkexec_path + " /usr/bin/dpm-query set default " + setting);
-    },
+        let dpmquery_path = GLib.find_program_in_path("dpm-query");
+        let pkexec_path = GLib.find_program_in_path("pkexec");
 
+        [res, pid, fdin, fdout, fderr] = GLib.spawn_async_with_pipes(null, [dpmquery_path, "test", "default"],
+                                                                     null, GLib.SpawnFlags.SEARCH_PATH, null);
+        let outstream = new Gio.UnixInputStream({fd:fdout,close_fd:true});
+        let stdout = new Gio.DataInputStream({base_stream: outstream});
+
+        let [out, size] = stdout.read_line(null);
+
+        if(out == null) {
+            this._iconActor.icon_name = "radeon-control-error";
+        }
+        else {
+            let result = out.toString();
+            if(result.search("forbidden") == -1) {
+                [res, pid] = GLib.spawn_async_with_pipes(null, [dpmquery_path, "set", "default"].concat(settings),
+                                                         null, GLib.SpawnFlags.SEARCH_PATH, null);
+                GLib.spawn_close_pid(pid);
+            }
+            else {
+                [res, pid] = GLib.spawn_async_with_pipes(null, [pkexec_path, dpmquery_path, "set", "default"].concat(settings),
+                                                         null, GLib.SpawnFlags.SEARCH_PATH, null);
+                GLib.spawn_close_pid(pid);
+            }
+        }
+        GLib.spawn_close_pid(pid);
+    },
 };
